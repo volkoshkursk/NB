838dcf058a2459dbfc38345e65f3dfc95becb043
volkoshkursk <zaz-pagani@inbox.ru>
2023-03-17 12:26:31 +0300

    begin adding new approach to mi calculation

--- mi1.cpp
+++ mi1.cpp
@@ -99,6 +99,82 @@ double mi(char** news, unsigned n2, char* class_, char** classes, char* word)
 //		PyList_Append(out, PyInt_FromLong(i));
 }
 
+#ifdef __cplusplus
+extern "C" double mi_slow(unsigned long long* news, unsigned n2, char* class_, char** classes, unsigned long long word)
+#else
+double mi(unsigned long long* news, unsigned n2, char* class_, char** classes, unsigned long long word)
+#endif
+{
+	long double N11 = 0;
+	long double N10 = 0;
+	long double N01 = 0;
+	long double N00 = 0;
+	for(unsigned j = 0; j < n2; j++)
+	{
+		if(strstr(classes[j], class_) != NULL)
+		{
+//			cout << "class+" << endl;
+			if(news[j] % word == 0) 
+			{
+				cout << "word+" << endl;
+				N11 ++;
+			}
+			else 
+			{
+				cout << "word-" << endl;
+				N01 ++;
+			}
+		}
+		else 
+		{
+//			cout << "class-" << endl;
+			if(news[j] % word == 0)  
+			{
+				cout << "word+" << endl;
+				N10 ++;
+			}
+			else 
+			{
+				cout << "word-" << endl;
+				N00 ++;
+			}
+		}
+//		cout<< "N11 " << N11<<endl;
+//		cout<< "N10 " << N10<<endl;
+//		cout<< "N01 " << N01<<endl;
+//		cout<< "N00 " << N00<<endl;
+	}
+	if((N11*N10*N00*N01) != 0)
+	{
+		long double N = N11+N10+N00+N01;
+		long double N1x = N11+N10;
+		long double Nx1 = N11 + N01;
+		long double N0x = N01+N00;
+		long double Nx0 = N10 + N00;
+//		cout<< "N " << N<<endl;
+//		cout<< "N1x " << N1x<<endl;
+//		cout<< "Nx1 " << Nx1<<endl;
+//		cout<< "N0x " << N0x<<endl;
+//		cout<< "Nx0 " << Nx0<<endl;
+//		cout << (N11/N) * log2((N*N11)/(N1x*Nx1)) << endl;
+//		cout << (N01/N) * log2((N*N01)/(N0x*Nx1))<< endl;
+//		cout << (N10/N) * log2((N*N10)/(N1x*Nx0)) << endl;
+//		cout << (N00/N) * log2((N*N00)/(N0x*Nx0)) << endl;
+//		cout << (((N11/N)*log2((N*N11)/(N1x*Nx1))) + ((N01/N) * log2((N*N01)/(N0x*Nx1))) + ((N10/N) * log2((N*N10)/(N1x*Nx0))) + ((N00/N) * log2((N*N00)/(N0x*Nx0))))<< endl << endl;
+		return (((N11/N)*logarithm((N*N11)/(N1x*Nx1))) + ((N01/N) * logarithm((N*N01)/(N0x*Nx1))) + ((N10/N) * logarithm((N*N10)/(N1x*Nx0))) + ((N00/N) * logarithm((N*N00)/(N0x*Nx0))));
+	}
+	else
+	{
+	    if (N11 == 0)
+		    return -1;
+		else
+		{
+		    
+		}
+	}
+//		PyList_Append(out, PyInt_FromLong(i));
+}
+
 int main()
 {
 	return 1;
